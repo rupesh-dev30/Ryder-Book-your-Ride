0b83694f0976745affa0fd139d26664284b0a00b
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-18 00:47:08 +0530

    swiper in welcome...

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,49 @@
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+
+const Onboarding = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(auth)/sign-up");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286ff] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex items-center justify-center">
+            <Image
+              source={item.image}
+              className="w-full h-[300px] mt-8"
+              resizeMode="contain"
+            />
+            <Text>{item.title}</Text>
+          </View>
+        ))}
+      </Swiper>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,8 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Home Screen</Text>
-    </SafeAreaView>
+    <Redirect href="/(auth)/welcome" />
   );
 };
 
